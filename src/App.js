b362f1f6cda18c5dbab0ef019c4b2db58b83c9cf
Timothee Odushina <hsp20tim@gmail.com>
2022-08-17 19:18:26 -0600

    added index

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Data from './data.json'
 
 
 
+
+
 function App() {
 
   return (
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -6,37 +6,13 @@ class HornedBeast extends React.Component {
   constructor(props) {
     console.log(props.title);
     super(props);
-
-    this.state = { count: 0 }
-  }
-
-  // increment the counter
-  increment = (e) => {
-    let nextState = {
-      count: this.state.count + 1
-    }
-    this.setState(nextState);
-    // this.setState( { count: this.state.count + 1 })
-    // this.setState( { count })
-  }
-
-  // decrement the counter
-  decrement = (e) => {
-    let count = this.state.count - 1;
-    console.log(this);
-    this.setState({ count });
   }
 
   render() {
     return (
-
-      // <>
-        
-      // </>
-
       <div>
         <h2>{this.props.title}</h2>
-        <img id={this.props.id} src={this.props.imageURL} titlte={this.props.imgTitle} alt={this.props.description} ></img>
+        <img id={this.props._id} src={this.props.imageURL} title={this.props.title} alt={this.props.description} keyword={this.props.keyword} horns={this.props.horns}></img>
         <p>{this.props.description}</p>
       </div>
     )
