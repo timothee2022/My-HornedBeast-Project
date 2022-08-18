f484292a7e4bbb647e8f7aef1fad02f108752886
Timothee Odushina <hsp20tim@gmail.com>
2022-08-18 07:27:35 -0600

    added Modal

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-// import Button from "react-bootstrap/Button";
+
 class HornedBeast extends React.Component {
 
 
@@ -17,28 +17,29 @@ class HornedBeast extends React.Component {
   render() {
     return (
 
+      <>
 
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title><h2>{this.props.title}</h2></Card.Title>
-          <img alt="" />
-          <Card.Img onClick={this.upvote}
-            src={this.props.imageURL}
-            alt={this.props.description}
-            title={this.props.title} />
-          <Card.Text>
-            {this.props.description}
-          </Card.Text>
-          <Card.Text>
-            Favorites: {this.state.votes}
-          </Card.Text>
-          {/* <Button variant="primary">Go somewhere</Button> */}
-        </Card.Body>
-      </Card>
-    );
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title><h2>{this.props.title}</h2></Card.Title>
+            <img alt="" />
+            <Card.Img onClick={this.upvote}
+              src={this.props.imageURL}
+              alt={this.props.description}
+              title={this.props.title} />
+            <Card.Text>
+              {this.props.description}
+            </Card.Text>
+            <Card.Text>
+              Favorites: {this.state.votes}
+            </Card.Text>
+            {/* <Button variant="primary">Go somewhere</Button> */}
+          </Card.Body>
+        </Card>
+        );
   }
 }
-
+      </>
 
 export default HornedBeast;
